6f237cfe57e835db7b308d3c4b9f10f70bd896ec
Ashraya Ananthanarayanan <ashraya@Ashrayas-Air.hsd1.pa.comcast.net>
2024-08-05 12:25:38 -0400

    Fit proxy function handling

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -4,8 +4,7 @@ const API_BASE_URL = 'https://api.zephyrscale.smartbear.com/v2';
 const API_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjb250ZXh0Ijp7ImJhc2VVcmwiOiJodHRwczovL2RpZ2l0YWwtc29sdXRpb25zLmF0bGFzc2lhbi5uZXQiLCJ1c2VyIjp7ImFjY291bnRJZCI6IjYzZDY0ZjQ5ZGI0ZjcxNWM5NzFlYmNhOCJ9fSwiaXNzIjoiY29tLmthbm9haC50ZXN0LW1hbmFnZXIiLCJzdWIiOiI0NjI1NDUyOC1kYjI4LTNiMTgtOGRkMi03ZGJjMmY2NWYzYjUiLCJleHAiOjE3NTI4MDQ0MjgsImlhdCI6MTcyMTI2ODQyOH0.AICGUijQnZLEfbpvd78f02PkEs8BP3hsKJLZ8R9vXFU';
 
 exports.handler = async (event) => {
-  const { path } = event;
-  const apiPath = path.replace('/.netlify/functions/proxy', '');
+  const apiPath = event.path.replace('/.netlify/functions/proxy', '');
   const url = `${API_BASE_URL}${apiPath}`;
 
   try {
